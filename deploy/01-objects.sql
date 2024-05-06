a595a80bcb74d2463683e5edd0f1529a676887a1
Andrew Carson <andrew.carson@snowflake.com>
2024-05-06 14:46:51 -0400

    fixed type

--- deploy/01-objects.sql
+++ deploy/01-objects.sql
@@ -1,6 +1,6 @@
-alter table pub_sap.employees ADD COLUMN AGEE NUMBER;
+alter table pub_sap.employees ADD COLUMN AGE NUMBER;
 
 create or replace view pub_sap.employees_vw as 
 select 
-    first_name,last_name, agee 
+    first_name,last_name, age
 from pub_sap.employees;
